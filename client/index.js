b14811a9c35990736ebda7e827094bf30fea33e3
Alexey Krasman <akrasman@gmail.com>
2024-07-04 08:43:37 +0300

    Refactoring (#5)

--- client/index.js
+++ client/index.js
@@ -1,10 +1,24 @@
-import { toggleLoading } from "./utils.js";
+import {
+  cardFormStyle,
+  modalEl,
+  renderResultMessage,
+  retryButtonEl,
+  submitButtonEl,
+  toggleLoading,
+} from './utils.js';
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const {session_id, backend_url} = await fetchConfig();
-  const client = await initializeClient(session_id, backend_url);
-  const cardHandler = await client.use("CARD");
-  const threeDSHandler = await client.use("3DS");
+let cardHolderField;
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const { session_id, backend_url } = await fetchConfig();
+
+  const { Hellgate } = window;
+  const client = await Hellgate.init(session_id, {
+    base_url: backend_url,
+    challenge_3ds_container: document.getElementById('challenge'),
+  });
+  const cardHandler = client.use('CARD');
+  const threeDSHandler = client.use('3DS');
 
   await setupCardForm(cardHandler);
   setup3DSHandler(threeDSHandler);
@@ -14,85 +28,36 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 // Function to fetch session ID and backend URL
-async function fetchConfig() {
-  const response = await fetch("/config");
-  const data = await response.json();
-  return data;
-}
-
-// Function to initialize Hellgate client
-async function initializeClient(session_id, backend_url) {
-  const { Hellgate } = window;
-  return await Hellgate.init(session_id, {
-    base_url: backend_url,
-    challenge_3ds_container: document.getElementById("challenge"),
-  });
+function fetchConfig() {
+  return fetch('/config').then(r => r.json());
 }
 
 // Function to setup event listeners for 3DS handler
 function setup3DSHandler(threeDSHandler) {
-  threeDSHandler.onStartChallenge(() => {
-    console.log("Challenge started");
-    document.getElementById("modal").style.display = "block";
-  });
-
-  threeDSHandler.onChallengeFinished(() => {
-    console.log("Challenge finished");
-    document.getElementById("modal").style.display = "none";
-  });
+  threeDSHandler.onStartChallenge(() => (modalEl.style.display = 'block'));
+  threeDSHandler.onChallengeFinished(() => (modalEl.style.display = 'none'));
 }
 
 // Function to create and mount card form and cardholder name field
 async function setupCardForm(cardHandler) {
-  const style = {
-    fonts: [
-      "https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap",
-    ],
-    base: {
-      fontFamily: "Nunito, sans-serif",
-      color: "rgb(14, 23, 38)",
-      fontSize: "16px",
-      fontWeight: 400,
-      "::placeholder": { color: "#6b7280" },
-      ":disabled": { color: "#aaa" },
-      padding: "0px",
-      backgroundColor: "transparent",
-    },
-    complete: { color: "cornflowerblue" },
-    invalid: { color: "lightcoral" },
-  };
-
-  const cardForm = await cardHandler.createForm({ style });
-  const cardHolder = cardHandler.createTextField({
-    placeholder: "Cardholder Name",
+  const cardForm = await cardHandler.createForm({ style: cardFormStyle });
+  cardHolderField = cardHandler.createTextField({
+    placeholder: 'Cardholder Name',
   });
 
   await Promise.all([
-    cardForm.mount("#card-form"),
-    cardHolder.mount("#cardholderName"),
+    cardForm.mount('#card-form'),
+    cardHolderField.mount('#cardholderName'),
   ]);
 }
 
-// Function to handle result response
-function displayResultMessage(result) {
-  const elemResult = document.getElementById("message");
-  const elemResultContainer = document.getElementById("challenge-result");
-  const containerElement = document.getElementById("container");
-  const { status, data } = result;
-  const isSuccess = status === "success";
-  const icon = isSuccess ? "check.svg" : "error.svg";
-  const htmlIcon = `
-  <div class="check">
-    <img src="./assets/svg/${icon}" />
-  </div>`;
-  document.getElementById("modal").style.display = "block";
-  
-  elemResult.innerHTML = isSuccess
-    ? `Tokenization completed.<br> ${htmlIcon} Token ID <br> ${data.token_id}`
-    : `Tokenization failed.<br> ${htmlIcon} ${data.message}`;
-
-  containerElement.style.display = "none";
-  elemResultContainer.style.display = "block";
+// Function to setup event listeners for buttons
+function setupButtonListeners(cardHandler) {
+  submitButtonEl.addEventListener('click', () => handleSubmit(cardHandler));
+  retryButtonEl.addEventListener('click', () => {
+    modalEl.style.display = 'none';
+    location.reload();
+  });
 }
 
 // Function to handle form submission
@@ -100,29 +65,13 @@ async function handleSubmit(cardHandler) {
   toggleLoading(true);
 
   try {
-    const additionalData = {
-      cardholder_name: document.getElementById("cardholderName").value,
-    };
-
-    const result = await cardHandler.process(additionalData);
-    displayResultMessage(result);
+    const result = await cardHandler.process({
+      cardholder_name: cardHolderField,
+    });
+    renderResultMessage(result);
   } catch (e) {
     console.log(e.message);
   }
 
   toggleLoading(false);
-}
-
-// Function to setup event listeners for buttons
-function setupButtonListeners(cardHandler) {
-  document
-    .getElementById("submit-button")
-    .addEventListener("click", async () => {
-      await handleSubmit(cardHandler);
-    });
-
-  document.getElementById("retry-button").addEventListener("click", () => {
-    document.getElementById("modal").style.display = "none";
-    location.reload();
-  });
 }
--- client/utils.js
+++ client/utils.js
@@ -1,11 +1,53 @@
-export function toggleLoading(boolean) {
-  const elemButton = document.getElementById("submit-button");
-  const elemCardData = document.getElementById("card-form");
-  const elemCardName = document.getElementById("cardholderName");
-  const elemLoading = document.getElementById("loading-dot");
+export const submitButtonEl = document.getElementById('submit-button');
+export const retryButtonEl = document.getElementById('retry-button');
+export const modalEl = document.getElementById('modal');
 
-  elemButton.style.display = boolean ? "none" : "block";
-  elemCardData.style.display = boolean ? "none" : "block";
-  elemCardName.style.display = boolean ? "none" : "block";
-  elemLoading.style.display = boolean ? "block" : "none";
+const cardDataEl = document.getElementById('card-form');
+const cardNameEl = document.getElementById('cardholderName');
+const loadingEl = document.getElementById('loading-dot');
+const resultEl = document.getElementById('message');
+const resultContainerEl = document.getElementById('challenge-result');
+const containerEl = document.getElementById('container');
+
+export const cardFormStyle = {
+  fonts: [
+    'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap',
+  ],
+  base: {
+    fontFamily: 'Nunito, sans-serif',
+    color: 'rgb(14, 23, 38)',
+    fontSize: '16px',
+    fontWeight: 400,
+    '::placeholder': { color: '#6b7280' },
+    ':disabled': { color: '#aaa' },
+    padding: '0px',
+    backgroundColor: 'transparent',
+  },
+  complete: { color: 'cornflowerblue' },
+  invalid: { color: 'lightcoral' },
+};
+
+export function toggleLoading(boolean) {
+  submitButtonEl.style.display = boolean ? 'none' : 'block';
+  cardDataEl.style.display = boolean ? 'none' : 'block';
+  cardNameEl.style.display = boolean ? 'none' : 'block';
+  loadingEl.style.display = boolean ? 'block' : 'none';
+}
+
+export function renderResultMessage(result) {
+  const { status, data } = result;
+  const isSuccess = status === 'success';
+  const icon = isSuccess ? 'check.svg' : 'error.svg';
+  const htmlIcon = `
+  <div class="check">
+    <img src="./assets/svg/${icon}" />
+  </div>`;
+  modalEl.style.display = 'block';
+
+  resultEl.innerHTML = isSuccess
+    ? `Tokenization completed.<br> ${htmlIcon} Token ID <br> ${data.token_id}`
+    : `Tokenization failed.<br> ${htmlIcon} ${data.message}`;
+
+  containerEl.style.display = 'none';
+  resultContainerEl.style.display = 'block';
 }
